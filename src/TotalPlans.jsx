2adfe63d8dbdbc698d877e72f577916ffb382614
Bessmack <jenwied4lif@gmail.com>
2025-07-22 22:52:21 +0300

    Added Deposit buttons and also added the deposit functionality inside the InitialDisplay compo

--- src/TotalPlans.jsx
+++ src/TotalPlans.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 
 function TotalPlans({goals}){
 
-    return <h2>You have {goals.length} Set Goals</h2>
+    return <h2>Goals: {goals.length}</h2>
 }
 
 export default TotalPlans;
